d0e2b2fbe7abfd144aa94ee922b7df847560e4a0
guilherme.nascimento1 <guilherme.nascimento@nava.com.br>
2022-08-10 15:15:57 -0300

    Many-to-many association with JoinTable

--- src/main/java/br/com/projectjpa/config/TestConfig.java
+++ src/main/java/br/com/projectjpa/config/TestConfig.java
@@ -51,6 +51,14 @@ public class TestConfig implements CommandLineRunner {
 
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+        p2.getCategories().add(cat3);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
     }
--- src/main/java/br/com/projectjpa/model/Category.java
+++ src/main/java/br/com/projectjpa/model/Category.java
@@ -1,5 +1,7 @@
 package br.com.projectjpa.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.HashSet;
@@ -16,7 +18,8 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
