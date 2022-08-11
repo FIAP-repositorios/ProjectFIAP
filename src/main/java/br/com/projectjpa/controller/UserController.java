e92aa724f2372c7baf9322d8f085789c84642df5
guilherme.nascimento1 <guilherme.nascimento@nava.com.br>
2022-08-11 15:22:07 -0300

    User update

--- src/main/java/br/com/projectjpa/controller/UserController.java
+++ src/main/java/br/com/projectjpa/controller/UserController.java
@@ -41,4 +41,10 @@ public class UserController {
         userService.delete(id);
         return ResponseEntity.noContent().build();
     }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+        obj = userService.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
 }
