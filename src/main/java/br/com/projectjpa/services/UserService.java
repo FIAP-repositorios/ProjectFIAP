ebfd27c850203759c90eac9ffe53f8cf005ca65f
guilherme.nascimento1 <guilherme.nascimento@nava.com.br>
2022-08-11 19:17:50 -0300

    Exception handling - Delete

--- src/main/java/br/com/projectjpa/services/UserService.java
+++ src/main/java/br/com/projectjpa/services/UserService.java
@@ -2,8 +2,11 @@ package br.com.projectjpa.services;
 
 import br.com.projectjpa.model.User;
 import br.com.projectjpa.repositories.UserRepository;
+import br.com.projectjpa.services.exceptions.DatabaseException;
 import br.com.projectjpa.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
---
+++ src/main/java/br/com/projectjpa/services/exceptions/DatabaseException.java
@@ -0,0 +1,11 @@
+package br.com.projectjpa.services.exceptions;
+
+import javax.xml.crypto.Data;
+
+public class DatabaseException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+}
