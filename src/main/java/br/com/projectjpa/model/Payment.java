f30aef2e5cd49c43178b660bdc3f308cc64d051c
guilherme.nascimento1 <guilherme.nascimento@nava.com.br>
2022-08-11 13:32:42 -0300

    Subtotal & Total methods

--- src/main/java/br/com/projectjpa/model/Payment.java
+++ src/main/java/br/com/projectjpa/model/Payment.java
@@ -1,5 +1,7 @@
 package br.com.projectjpa.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
@@ -15,6 +17,7 @@ public class Payment implements Serializable {
     private Long id;
     private Instant moment;
 
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
