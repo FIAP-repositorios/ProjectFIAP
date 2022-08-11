d63bcf5ac5effc9104fbfe90e34d7ec8057fba68
guilherme.nascimento1 <guilherme.nascimento@nava.com.br>
2022-08-11 11:18:22 -0300

    Product-OrderItem one-to-many association

--- src/main/java/br/com/projectjpa/model/Product.java
+++ src/main/java/br/com/projectjpa/model/Product.java
@@ -1,5 +1,7 @@
 package br.com.projectjpa.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.HashSet;
@@ -24,6 +26,9 @@ public class Product implements Serializable {
             joinColumns = @JoinColumn(name = "product_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {
     }
@@ -75,9 +80,17 @@ public class Product implements Serializable {
     public void setImgUrl(String imgUrl) {
         this.imgUrl = imgUrl;
     }
-
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x : items) {
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     @Override
