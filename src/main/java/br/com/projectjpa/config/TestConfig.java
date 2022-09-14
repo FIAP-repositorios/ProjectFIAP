32390bb07ba53d6cba23a47c68dfdea59a56e918
Guilherme <guilhermemeirelesnascimento@gmail.com>
2022-09-14 20:27:56 -0300

    FIX conteúdo nas tabelas

--- src/main/java/br/com/projectjpa/config/TestConfig.java
+++ src/main/java/br/com/projectjpa/config/TestConfig.java
@@ -6,13 +6,11 @@ import br.com.projectjpa.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import java.time.Instant;
 import java.util.Arrays;
 
 @Configuration
-@Profile("test")
 public class TestConfig implements CommandLineRunner {
 
     @Autowired
