2dd5174587c1b56297f504fb86269397d8eefecd
Rfontt <ritafontenele@ext.cvccorp.com.br>
2022-09-16 18:31:01 -0300

    Adicionando método de salvar em orders

--- src/main/java/br/com/projectjpa/controller/OrderController.java
+++ src/main/java/br/com/projectjpa/controller/OrderController.java
@@ -1,17 +1,14 @@
 package br.com.projectjpa.controller;
 
 import br.com.projectjpa.entities.MessagePattern;
+import br.com.projectjpa.exceptions.AlreadyExistsException;
 import br.com.projectjpa.exceptions.InternalServerErrorException;
-import br.com.projectjpa.exceptions.NotFoundException;
 import br.com.projectjpa.model.Order;
 import br.com.projectjpa.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -44,16 +41,40 @@ public class OrderController {
         try {
             Order order = orderService.findById(id);
 
+            if (order == null) {
+                return new ResponseEntity(
+                        new MessagePattern("Order dont exists!"),
+                        HttpStatus.NOT_FOUND
+                );
+            }
+
             return new ResponseEntity(
                     order,
                     HttpStatus.OK
             );
-        } catch (NotFoundException error) {
+        } catch (Exception error) {
+            return new ResponseEntity(
+                    new MessagePattern("Internal server error \n" + error.getMessage()),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity save(@RequestBody Order order) {
+        try {
+            Order orderCreated = orderService.save(order);
+
+            return new ResponseEntity(
+                    orderCreated,
+                    HttpStatus.OK
+            );
+        } catch (AlreadyExistsException error) {
             return new ResponseEntity(
                     new MessagePattern(error.getMessage()),
-                    HttpStatus.NOT_FOUND
+                    HttpStatus.CONFLICT
             );
-        } catch (Exception error) {
+        } catch (InternalServerErrorException error) {
             return new ResponseEntity(
                     new MessagePattern("Internal server error \n" + error.getMessage()),
                     HttpStatus.INTERNAL_SERVER_ERROR
