67377e4a5b47d810b432c7a10a1bdc2f642e0052
Guilherme <guilhermemeirelesnascimento@gmail.com>
2022-09-16 11:46:33 -0300

    FIX Jackson Error III

--- src/main/java/br/com/projectjpa/model/Order.java
+++ src/main/java/br/com/projectjpa/model/Order.java
@@ -2,9 +2,7 @@ package br.com.projectjpa.model;
 
 import br.com.projectjpa.model.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -13,11 +11,9 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-@NoArgsConstructor
-@Getter
-@Setter
 @Entity
 @Table(name = "tb_order")
+@NoArgsConstructor
 public class Order implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -45,10 +41,50 @@ public class Order implements Serializable {
         this.client = client;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
     public void setOrderStatus(OrderStatus orderStatus) {
         if (orderStatus != null) {
             this.orderStatus = orderStatus.getCode();
         }
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
     }
 
     public Double getTotal() {
